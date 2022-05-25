aaf8861ee3edaf9f80d266091a63887cadd638d7
Cristine Paula Marquez Santiago <cmarquezs2021@cepnet.net>
2022-05-25 02:50:46 +0200

    video

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from "vuetify";
+import 'vuetify/dist/vuetify.min.css'; // vuetify style css
 
 Vue.config.productionTip = false
+Vue.use(Vuetify);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  vuetify: new Vuetify(), // to access $vuetify inside your Vue components
+}).$mount("#app");
